380a4c1eb8502517d00cd5784f8dc330e914d05e
Zach <mcnultzd198@potsdam.edu>
2014-10-08 20:26:56 -0400

    nothing changed here

--- 100Monsters.py
+++ 100Monsters.py
@@ -13,6 +13,7 @@ from pandac.PandaModules import *
 from direct.actor.Actor import Actor
 from math import fabs
 from player import Player
+from monster import Monster
 
 class Monsters(object):
 
@@ -25,9 +26,14 @@ class Monsters(object):
         self.loadLevel()
         self.initPlayer()
 
+        #used for debugging
+        self.initMonster()
+
     def initCollision(self):
         base.cTrav = CollisionTraverser()
         base.pusher = CollisionHandlerPusher()
+        #for debugging purposes
+        base.cTrav.showCollisions(render)
 
     def loadLevel(self):
         self.level = loader.loadModel("resources/levels/lobby4.egg")
@@ -37,5 +43,7 @@ class Monsters(object):
     def initPlayer(self):
         self.node = Player()
 
+    def initMonster(self):
+        self.node = Monster()
 Monsters()
 run()
