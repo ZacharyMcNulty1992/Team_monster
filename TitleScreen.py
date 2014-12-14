fb7f2d2db584b8c494eb90e8f726e41f8d1ce4ec
Calicara <katgirl900214@cpe-72-230-77-79.twcny.res.rr.com>
2014-12-14 16:40:44 -0500

    TitleScreen pops up now. Still having trouble loading custom buttons. Curent buttons don't do anything, but I am working on fixing that.

--- TitleScreen.py
+++ TitleScreen.py
@@ -1,35 +1,51 @@
 from direct.showbase.ShowBase import ShowBase
-from direct.directbase import DirectStart
-from direct.gui.OnscreenText import OnscreenText
+import direct.directbase.DirectStart
+from direct.gui.DirectGui import *
 from direct.gui.DirectButton import DirectButton
-from panda3d.core import TextNode
 from direct.gui.DirectFrame import DirectFrame
+from panda3d.core import NodePath, PandaNode
+from direct.gui.OnscreenImage import OnscreenImage
+from direct.showbase.DirectObject import DirectObject
 
 
- 
-class TitleScreen:
+class Titlescreen(ShowBase):
+  
+  startPressed = False
+  screen = 0
+  b = 0
 
-	startPressed = False
- 	screen = 0
- 	b = 0
- 	
+
+
     #This sets the size of the screen
-def __init__(self, xSize, ySize):
-    self.screen = DirectFrame(frameSize=(xSize/2, xSize/2, ySize/2, ySize/2),	frameColor=(0, 0, 0, 1))
+  def __init__(self, xSize, ySize):
+      self.screen = DirectFrame(frameSize=(xSize/2, xSize/2, ySize/2, ySize/2),	frameColor=(0, 0, 0, 1))
+  
+  
+  
     #This plays the music
-    self.initMusic()
-    #This Creates a button
-    self.b = DirectButton(text=("Click me"), scale=.1)
-    b['command'] = self.buttonClicked
-    
-	# Font Header
-    self.text = TextNode('New_Text')
-    self.text.setText("100 Monsters")
-    textNodePath = render2d.attachNewNode(self.text)
-    textNodePath.setScale(0.07)
-    self.text.setAlign(TextNode.ABoxedCenter)
-    Zccara = loader.loadFont('resources/fonts/CTS.ttf')
-    self.text.setFont(Zccara)
-    textNodePath.setPos(0, 0, .60)
-    self.text.setWordwrap(20)
-    self.text.setAlign(self.text.ACenter)
+  def __init__(self):
+      self.mySound = base.loader.loadSfx("resources/music/LooseSpirits.ogg")
+      self.mySound.play()
+
+      OnscreenImage(image = "logo.png", pos = (0.0, -70.0, 0.45), scale = (1.1, 0.9, 0.3))
+
+      start = DirectButton(text = ("Start!", "Start!", "Hover", "disabled"), scale = 0.09, pos = (0,0,-.1))
+      quit = DirectButton(text = ("Quit", "Quit", "Hover", "disabled"), scale = 0.09, pos = (0,0,-.3))
+
+#This Creates the play button
+#  def __init__(self):
+#    self.maps = loader.loadModel('resources/Buttons/EGG_Play_Files/button_maps.egg')
+#    self.play = DirectButton(geom = (self.maps.find('Play.png/button_ready'),
+#    self.maps.find('Play_Click.png/button_click'),
+#    self.maps.find('Play_Hover.png/button_rollover'),
+#    self.maps.find('Play_Click.png/button_disabled')))
+#This Creates the quit button
+#def __init__(self):
+#     self.maps = loader.loadModel('resources/Buttons/EGG_Quit_Files/button_maps.egg')
+#     self.quit = DirectButton(geom = (maps.find('Quit.png/button_ready'),
+#     maps.find('Quit_Click.png/button_click'),
+#     maps.find('Quit_Hover.png/button_rollover'),
+#     maps.find('Quit_Click.png/button_disabled')))
+
+t = Titlescreen()
+run()
